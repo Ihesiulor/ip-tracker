35df816f51d390cc784636617939fa980308f5ab
Ihesiulor <dannykidm@outlook.com>
2021-05-31 18:14:36 +0000

    fixes eslint bug

--- src/components/map.js
+++ src/components/map.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useRef, useEffect, useContext } from "react";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import mapboxgl from "!mapbox-gl";
